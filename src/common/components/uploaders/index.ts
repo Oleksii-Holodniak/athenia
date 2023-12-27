49c13a57924105ec2daefbc512a812c56275ba33
Oleksii Holodniak <holodniakwork@gmail.com>
2023-12-28 01:14:06 +0200

    AT-details form add meterials hot fix

--- src/common/components/uploaders/index.ts
+++ src/common/components/uploaders/index.ts
@@ -1,2 +1,2 @@
-export { default as DropFiles } from "./DropFiles/DropFiles";
+export { default as DropFiles } from "./DropFsles/DropFiles";
 export { default as FilesList } from "./FilesList/FilesList";
