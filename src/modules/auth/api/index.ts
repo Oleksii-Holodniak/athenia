a66888375708a4d480086a00cd2ca762e8d9d110
Oleksii Holodniak <holodniakwork@gmail.com>
2023-12-29 17:08:30 +0200

    VERY VERY HOT FIX

--- src/modules/auth/api/index.ts
+++ src/modules/auth/api/index.ts
@@ -1,12 +1,16 @@
 import axios from "@/services/base";
+import Cookies from "cookies-js";
 import { ILoginFormValues, IRegisterApi } from "../types";
 export const AuthService = {
   async register(form: IRegisterApi) {
     const data = await axios.post("/auth/signup", form);
+    console.log("data :", data);
     return data;
   },
   async login(form: ILoginFormValues) {
     const data = await axios.post("/auth/signin", form);
+    const cookie = Cookies.get(process.env.COOKIES_NAME!);
+    console.log("cookie :", cookie);
     return data;
   },
 };
